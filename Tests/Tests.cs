19aa4cdce53dba82aaf232e1c5c2449c4da91d95
Simon Cropp <simon.cropp@gmail.com>
2021-03-08 11:13:22 +1100

    docs and tests

---
+++ Tests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Score.Commands;
+using Score.Models;
+using Score.Services;
+using Score.Utilities;
+using Spectre.Console;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class Tests
+{
+    [Fact]
+    public async Task DumpScore()
+    {
+        var settings = new ScoreCommand.Settings {PackageName = "Newtonsoft.Json", PackageVersion = "12.0.3"};
+        var nuGetService = new NuGetService();
+        var packageContext = new PackageContext(settings);
+        packageContext.NuspecReader = await nuGetService.GetNuspecFromPackage(packageContext);
+        packageContext.PackageMetadata = await nuGetService.GetNuGetPackageMetadataFromPackage(packageContext);
+
+        var scoreService = new ScoreService();
+        var score = await scoreService.ScorePackage(packageContext);
+
+        AnsiConsole.Record();
+        ScoreDumper.DumpScore(score);
+
+        await Verifier.Verify(AnsiConsole.ExportText());
+    }
+}
